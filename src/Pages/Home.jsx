6d057a17b39b35e381a99097911a2ea60f012ee3
hossainshofiq <ah731710@gmail.com>
2025-03-10 02:17:49 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='my-5 space-y-5 text-center'>
+            <h1>This is home</h1>
+            <section className='flex justify-center gap-5'>
+                <button className="btn btn-neutral">Neutral</button>
+                <button className="btn btn-primary">Primary</button>
+                <button className="btn btn-secondary">Secondary</button>
+                <button className="btn btn-accent">Accent</button>
+                <button className="btn btn-info">Info</button>
+                <button className="btn btn-success">Success</button>
+                <button className="btn btn-warning">Warning</button>
+                <button className="btn btn-error">Error</button>
+            </section>
+            <section>
+                <div
+                    className="hero min-h-screen"
+                    style={{
+                        backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                    }}>
+                    <div className="hero-overlay"></div>
+                    <div className="hero-content text-neutral-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                            <p className="mb-5">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            </p>
+                            <button className="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
